b0a082f35afb9c698c60a9c4472118b5c6e6f78f
Duy Vu <ducduy23899@gmail.com>
2019-05-03 15:15:53 +0700

    Commit final

--- src/PacMangame/GameFrame.java
+++ src/PacMangame/GameFrame.java
@@ -184,7 +184,7 @@ class GamePanel extends JPanel {
             g.drawString("YOU LOSE", 75, 100);
             g.setColor(Color.cyan);
             g.setColor(Color.red);
-            g.drawString("Score " + score, 130, 320);
+            g.drawString("Score " + score, 120, 330);
             g.setFont(new Font("Arial", Font.BOLD, 20));
             g.drawString("Space to restart", 180, 200);
         } else { // normal
